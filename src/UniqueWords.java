e17826c5c5ed13cda8413b8aa5189008e5ea75f4
jacob <jacob.montufar23@my.stjohns.edu>
2025-02-27 22:35:58 -0500

    i added boolean and a few if statements

--- src/UniqueWords.java
+++ src/UniqueWords.java
@@ -9,16 +9,28 @@ public class UniqueWords
    */
    public static int countUnique(ArrayList<String> list)
    {
-	  int count = 0;
-	  
-      for (int i = 0; i < list.size(); i++)
-      {		 for (int j = 0; j < list.size(); j++)
-		 {
-			
-		 }
+   int count = 0; 
+   
+   
+   for (int i = 0; i < list.size(); i++) {
+      boolean isUnique = true; // Assume current element is unique
+      
+      // Check if the current element has appeared before
+      for (int j = 0; j < i; j++) {
+         if (list.get(i).equals(list.get(j))) { // If duplicate found
+            isUnique = false; // Mark as not unique
+            break; // Exit inner loop early
+         }
       }
-	  return count;
+      
+      // If the element is unique, increase the count
+      if (isUnique) {
+         count++;
+      }
    }
+   return count; // Return the total count of unique elements
+}
+
 
    public static void main(String[] args)
    {
